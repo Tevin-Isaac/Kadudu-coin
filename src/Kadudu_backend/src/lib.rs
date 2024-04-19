475e90b2a7f87f97d2f57f84b075f85939388c74
farzeenkist <farzeenkistnareddy@gmail.com>
2024-04-19 14:46:08 +0400

    Some types changes to better suit project

--- src/Kadudu_backend/src/lib.rs
+++ src/Kadudu_backend/src/lib.rs
@@ -1,25 +1,25 @@
 use std::collections::HashMap;
 use candid::CandidType;
 use candid::Principal;
-#[derive(CandidType, Default)]
+#[derive(CandidType)]
 struct ICRC1MemeToken {
     id: u64,
     name: String,
     symbol: String,
-    total_supply: u64,
-    ledger: HashMap<Principal, u64>,
+    total_supply: u128,
+    ledger: HashMap<Principal, u128>,
     meme_url: String,
     meme_description: String,
-    meme_creator: String,
+    meme_creator: Principal,
 }
 
 impl ICRC1MemeToken {
     pub fn new(
         id: u64,
-        total_supply: u64,
+        total_supply: u128,
         meme_url: String,
         meme_description: String,
-        meme_creator: String,
+        meme_creator: Principal,
     ) -> Self {
         ICRC1MemeToken {
             id,
@@ -33,10 +33,10 @@ impl ICRC1MemeToken {
         }
     }
 
-    pub fn transfer(&mut self, from: Principal, to: Principal, amount: u64) -> Result<(), String> {
+    pub fn transfer(&mut self, from: Principal, to: Principal, amount: u128) -> Result<(), String> {
         if let Some(balance) = self.ledger.get(&from) {
             if *balance >= amount {
-                let new_balance: u64 = balance - amount;
+                let new_balance: u128 = balance - amount;
                 self.ledger.insert(to, new_balance);
                 Ok(())
             } else {
@@ -47,18 +47,18 @@ impl ICRC1MemeToken {
         }
     }
 
-    pub fn balance_of(&self, account: Principal) -> u64 {
-        let amount :u64 = *self.ledger.get(&account).unwrap_or(&0);
+    pub fn balance_of(&self, account: Principal) -> u128 {
+        let amount :u128 = *self.ledger.get(&account).unwrap_or(&0);
         amount
     }
 
-    pub fn mint(&mut self, to: Principal, amount: u64) {
-        let balance :u64 = *self.ledger.get(&to).unwrap_or(&0);
+    pub fn mint(&mut self, to: Principal, amount: u128) {
+        let balance :u128 = *self.ledger.get(&to).unwrap_or(&0);
         self.ledger.insert(to, balance + amount);
         self.total_supply += amount;
     }
 
-    pub fn burn(&mut self, from: Principal, amount: u64) -> Result<(), String> {
+    pub fn burn(&mut self, from: Principal, amount: u128) -> Result<(), String> {
         if let Some(balance) = self.ledger.get_mut(&from) {
             if *balance >= amount {
                 *balance -= amount;
@@ -72,8 +72,8 @@ impl ICRC1MemeToken {
         }
     }
 
-    pub fn get_meme_creator(&self) -> String {
-        self.meme_creator.clone()
+    pub fn get_meme_creator(&self) -> Principal {
+        self.meme_creator
     }
 
     pub fn get_meme_description(&self) -> String {
